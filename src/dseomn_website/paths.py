5b01f0d5253f5bf154fe59a5bea8ea8cee3f3789
David Mandelberg <david@mandelberg.org>
2025-06-17 15:00:59 -0400

    Add function to convert a URL path to an FS path

---
+++ src/dseomn_website/paths.py
@@ -0,0 +1,22 @@
+# SPDX-FileCopyrightText: 2025 David Mandelberg <david@mandelberg.org>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import pathlib
+
+OUTPUT = pathlib.PurePath("output")
+
+
+def from_url_path(
+    url_path: str,
+    *,
+    dir_index: str = "index.html",
+) -> pathlib.PurePath:
+    """Returns an fs path from a url path."""
+    if not url_path.startswith("/"):
+        raise NotImplementedError("Relative url paths are not supported.")
+    relative = url_path.removeprefix("/")
+    if relative.endswith("/"):
+        return OUTPUT / relative / dir_index
+    else:
+        return OUTPUT / relative
---
+++ src/dseomn_website/paths_test.py
@@ -0,0 +1,25 @@
+# SPDX-FileCopyrightText: 2025 David Mandelberg <david@mandelberg.org>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import pathlib
+
+import pytest
+
+from dseomn_website import paths
+
+
+def test_from_url_path_error() -> None:
+    with pytest.raises(NotImplementedError, match="Relative url paths"):
+        paths.from_url_path("foo")
+
+
+@pytest.mark.parametrize(
+    "url_path,expected",
+    (
+        ("/foo/", "output/foo/index.html"),
+        ("/foo", "output/foo"),
+    ),
+)
+def test_from_url_path(url_path: str, expected: str) -> None:
+    assert paths.from_url_path(url_path) == pathlib.PurePath(expected)
