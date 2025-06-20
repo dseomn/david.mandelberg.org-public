2cc809a0fa13393e06a271007e70528375e05449
David Mandelberg <david@mandelberg.org>
2025-06-19 21:32:51 -0400

    Add arg to modify heading levels
    Blog posts should start at <h1> when they're on their own page, but at
    <h2> when included in a list page that has its own <h1>.

--- src/dseomn_website/headings.py
+++ src/dseomn_website/headings.py
@@ -5,11 +5,25 @@
 import lxml.html
 
 
-def article(html: str, /) -> str:
+def article(
+    html: str,
+    /,
+    offset: int = 0,
+) -> str:
     """Adjusts the headings of an <article>."""
     parsed = lxml.html.fragment_fromstring(html)
-    for heading_level in range(1, 7):
-        for element in parsed.cssselect(f"h{heading_level}"):
+    headings_by_level = {
+        level: tuple(parsed.cssselect(f"h{level}")) for level in range(1, 7)
+    }
+    for heading_level, elements in headings_by_level.items():
+        if not elements:
+            continue
+        if not 1 <= heading_level + offset <= 6:
+            raise ValueError(
+                f"Can't adjust h{heading_level} by offset {offset}."
+            )
+        for element in elements:
+            element.tag = f"h{heading_level + offset}"
             element.classes.add(f"h{heading_level}")
     return lxml.html.tostring(parsed, encoding="unicode")
 
--- src/dseomn_website/headings_test.py
+++ src/dseomn_website/headings_test.py
@@ -7,38 +7,59 @@ import pytest
 from dseomn_website import headings
 
 
-def test_article() -> None:
-    assert headings.article(
-        "".join(
-            (
-                "<article>",
-                "<h1>H 1</h1>",
-                "<h2>H 2</h2>",
-                "<h3>H 3</h3>",
-                "<div>",
-                "<h4>H 4</h4>",
-                "<h5>H 5</h5>",
-                "<h6>H 6</h6>",
-                "<p>P</p>",
-                "</div>",
-                "</article>",
-            )
-        )
-    ) == "".join(
+@pytest.mark.parametrize(
+    "html,offset,error_regex",
+    (
+        ("<h1>foo</h1>", -1, r"adjust h1 by offset -1"),
+        ("<h6>foo</h6>", 1, r"adjust h6 by offset 1"),
+    ),
+)
+def test_article_error(html: str, offset: int, error_regex: str) -> None:
+    with pytest.raises(ValueError, match=error_regex):
+        headings.article(html, offset=offset)
+
+
+@pytest.mark.parametrize(
+    "html,offset,expected",
+    (
         (
-            "<article>",
-            '<h1 class="h1">H 1</h1>',
-            '<h2 class="h2">H 2</h2>',
-            '<h3 class="h3">H 3</h3>',
-            "<div>",
-            '<h4 class="h4">H 4</h4>',
-            '<h5 class="h5">H 5</h5>',
-            '<h6 class="h6">H 6</h6>',
-            "<p>P</p>",
-            "</div>",
-            "</article>",
-        )
-    )
+            "".join(
+                (
+                    "<article>",
+                    "<h1>H 1</h1>",
+                    "<h2>H 2</h2>",
+                    "<h3>H 3</h3>",
+                    "<div>",
+                    "<h4>H 4</h4>",
+                    "<h5>H 5</h5>",
+                    "<h6>H 6</h6>",
+                    "<p>P</p>",
+                    "</div>",
+                    "</article>",
+                )
+            ),
+            0,
+            "".join(
+                (
+                    "<article>",
+                    '<h1 class="h1">H 1</h1>',
+                    '<h2 class="h2">H 2</h2>',
+                    '<h3 class="h3">H 3</h3>',
+                    "<div>",
+                    '<h4 class="h4">H 4</h4>',
+                    '<h5 class="h5">H 5</h5>',
+                    '<h6 class="h6">H 6</h6>',
+                    "<p>P</p>",
+                    "</div>",
+                    "</article>",
+                )
+            ),
+        ),
+        ("<h1>foo</h1>", 1, '<h2 class="h1">foo</h2>'),
+    ),
+)
+def test_article(html: str, offset: int, expected: str) -> None:
+    assert headings.article(html, offset=offset) == expected
 
 
 @pytest.mark.parametrize(
