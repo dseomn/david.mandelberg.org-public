c1b36ad1109bd0279313aebbc8f6e3de4a790406
David Mandelberg <david@mandelberg.org>
2025-06-27 21:13:58 -0400

    Move layout constants to python
    I'm planning to use them in python code, which can't easily import
    jinja.

---
+++ src/dseomn_website/layout.py
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: 2025 David Mandelberg <david@mandelberg.org>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+# When changing these values, also check if the image sizes in
+# include/media.jinja need changing too.
+MAIN_COLUMN_MAX_INLINE_SIZE = "60em"
+MAIN_COLUMN_PADDING_INLINE = "1em"
